a564204c2499ec4b4ef923338f1a11eed412bca3
Jason Cross <jason.garnet.cross@gmail.com>
2024-10-27 15:47:55 -0300

    Initial Commit

---
+++ ClockSliderLibraryClientMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClockSliderLibraryClientMac
+//
+//  Created by Jason Cross on 2024-10-27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
